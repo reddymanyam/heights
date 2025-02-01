8154a4145e0651ad9f61b25e13ac91492e4dc453
reddymanyam <reddappareddy18@gmail.com>
2025-02-01 23:15:43 +0530

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 // import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Heights from './Heights';
 import EmployeDetails from './EmployeDetails';
-<<<<<<< HEAD
 
-=======
->>>>>>> 4720c89441537a8e31e2e56b24cce09660291378
 // const theme = createTheme();
 
 function App() {
   return (
-<<<<<<< HEAD
     // <ThemeProvider theme={theme}>
     //   <Heights />
     // </ThemeProvider>
@@ -17,11 +13,6 @@ function App() {
       <EmployeDetails />
     </>
 
-=======
-    <>
-    <EmployeDetails />
-    </>
->>>>>>> 4720c89441537a8e31e2e56b24cce09660291378
   );
 }
 
